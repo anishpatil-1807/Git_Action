98b4561e4a8be8bd10f9b9fb341894e732857ef8
anishpatil-1807 <anishpatil0718@gmail.com>
2025-08-04 09:58:01 +0530

    update 3

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1> Hi this is Anish !!</h1>
+      <h1> How was your day ?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
